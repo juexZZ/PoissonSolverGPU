2371fb28e033c1b4095f5811ebfec816c723ccb9
一洧 邵 <947597028@qq.com>
2022-04-12 00:29:50 -0400

    Revert "system matrix finished, rhs assembler left"
    This reverts commit 8910a0d0005f61854ca89bfd95a90f840342885b.

--- src/solver/poissonPre.h
+++ src/solver/poissonPre.h
@@ -20,7 +20,7 @@ public:
 	MatrixXd _zDiff;
 };
 
-class PoissonProblem {
+class Problem {
 public:
 	int xlength=0; 
 	int ylength=0;
@@ -28,8 +28,8 @@ public:
 	double dx=1, dy=1, dz=1;
 	bool is_2d = true;
 	vector<BoundaryCondition> bc;
-	PoissonProblem() {};
-	PoissonProblem(int x, int y,int z, bool _is_2d) {
+	Problem() {};
+	Problem(int x, int y,int z, bool _is_2d) {
 		xlength = x;
 		ylength = y;
 		zlength = z;
@@ -37,7 +37,7 @@ public:
 	}
 	//Check condition if it is capable
 	//Every boundary should have a conition,if not return -1
-	//If all conditions are cauchy condition, then return 2, in this case we should fix the (0,0)/(0,0,0) to 0
+	//If all conditions are cauchy condition, then return 2, in this case we should fix the (0,0) to 0
 	//If it is all fine, then return 1
 	int conditionCheck() {
 		if (is_2d)
@@ -78,7 +78,6 @@ public:
 
 		}
 		else
-		//3D problem
 		{
 			vector<bool> _Bd{ false,false,false,false,false,false };
 			bool exist_Dirichlet = false;
@@ -117,7 +116,7 @@ public:
 	}
 };
 
-void preConditioner(PoissonProblem pro,MatrixXd &output,VectorXd &rhs) {
+void preConditioner(Problem pro,MatrixXd &output,VectorXd &rhs) {
 	//The poisson preconditioner
 	int conCheck = pro.conditionCheck();
 	if (conCheck==-1)
@@ -137,111 +136,22 @@ void preConditioner(PoissonProblem pro,MatrixXd &output,VectorXd &rhs) {
 		D = D + 4 * MatrixXd::Identity(pro.xlength-2,pro.xlength-2);
 		output.resize((pro.xlength - 2)*(pro.ylength-2), (pro.xlength - 2)* (pro.ylength - 2));
 		output.setZero();
-		for (size_t i = 0; i < pro.ylength-3; i++)
+		for (size_t i = 0; i < pro.ylength-2; i++)
+			for (size_t j = 0; j < pro.ylength-2; j++)
 		{	
-			//The matrix is (m-2)*(n-2)
-				output.block(i*(pro.xlength-2), i * (pro.xlength - 2), pro.xlength - 2, pro.xlength - 2)=D;
-				output.block((i+1) * (pro.xlength - 2), i * (pro.xlength - 2), pro.xlength - 2, pro.xlength - 2) = -MatrixXd::Identity(pro.xlength - 2, pro.xlength - 2);
-				output.block(i * (pro.xlength - 2), (i+1) * (pro.xlength - 2), pro.xlength - 2, pro.xlength - 2) = -MatrixXd::Identity(pro.xlength - 2, pro.xlength - 2);
-		}
-		output.block((pro.ylength - 2) * (pro.xlength - 2), (pro.ylength - 2) * (pro.xlength - 2), pro.xlength - 2, pro.xlength - 2) = D;
-		MatrixXd temp((pro.xlength - 2) * (pro.ylength - 2), (pro.xlength - 2) * (pro.ylength - 2));
-		for (size_t i = 0; i < pro.xlength - 2; i++)
-		{
-			bool xub = (i < pro.xlength - 3);
-			bool xlb = (i > 1);
-			for (size_t j = 0; j < pro.ylength - 2; j++)
-			{
-				temp(i * j, i * j) = 4;				
-				bool yub = (j < pro.ylength - 3);
-				bool ylb = (j > 1);
-				if (ylb)
-				{
-					temp(i * j, i * (j - 1)) = -1;
-				}
-				if (yub)
-				{
-					temp(i * j, i * (j + 1)) = -1;
-				}
-				if (xub)
-				{
-					temp(i * j, (i + 1) * j) = -1;
-				}
-				if (xlb)
-				{
-					temp(i * j, (i - 1) * j) = -1;
-				}	
-			}
-		}
-		assert(output == temp);
+			//The matrix is (m-2)*n-2
 
+		}
 	}
 	else 
 	{
 		output.resize((pro.xlength - 2) * (pro.ylength - 2)*(pro.zlength-2), (pro.xlength - 2) * (pro.ylength - 2)*(pro.zlength-2));
-		for (size_t i = 0; i < pro.xlength - 2; i+ pro.xlength - 3)
-		{
-			bool xub = (i < pro.xlength - 3);
-			bool xlb = (i > 1);
-			for (size_t j = 0; j < pro.ylength - 2; j+pro.ylength - 3)
-			{
-				bool yub = (j < pro.ylength - 3);
-				bool ylb = (j > 1);
-				for (size_t k = 0; k < pro.zlength-2; k+ pro.zlength-3)
-				{
-					bool zub = (k < pro.zlength - 3);
-					bool zlb = (k > 1);
-					output(i * j * k,i*j*k) = 6;
-					if (ylb)
-					{
-						output(i * j*k, i * (j - 1)*k) = -1;
-					}
-					if (yub)
-					{
-						output(i * j*k, i * (j + 1)*k) = -1;
-					}
-					if (xub)
-					{
-						output(i * j*k, (i + 1) * j*k) = -1;
-					}
-					if (xlb)
-					{
-						output(i * j*k, (i - 1) * j*k) = -1;
-					}
-					if (zub)
-					{
-						output(i * j * k, i * j * (k+1)) = -1;
-					}
-					if (zlb)
-					{
-						output(i * j * k, i * j * (k-1)) = -1;
-					}
-				}
-			}
-		}
 
-		for (size_t i = 1; i < pro.xlength - 3; i++)
-		{
-			for (size_t j = 1; j < pro.ylength - 3; j++)
-			{
-				for (size_t k = 1; k < pro.zlength-3; k++)
-				{
-					output(i * j * k, i * j * k) = 6;
-					output(i * j * k, i * (j - 1) * k) = -1;
-					output(i * j * k, i * (j + 1) * k) = -1;
-					output(i * j * k, (i + 1) * j * k) = -1;
-					output(i * j * k, (i - 1) * j * k) = -1;
-					output(i * j * k, i * j * (k + 1)) = -1;
-					output(i * j * k, i * j * (k - 1)) = -1;
-				}
-			}
-		}
 	}
-
 	
 }
 
-void conjugateGradient(PoissonProblem pro, VectorXd result) {
+void conjugateGradient(Problem pro, VectorXd result) {
 	//Solving problems AX=B
 	MatrixXd A;
 	VectorXd B,X;
