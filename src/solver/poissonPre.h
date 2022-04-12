1c4270c50585142e8226cdf0cebac20d5e9907ea
njsyw1997 <44545837+njsyw1997@users.noreply.github.com>
2022-04-12 15:25:28 -0400

    rhs

--- src/solver/poissonPre.h
+++ src/solver/poissonPre.h
@@ -20,16 +20,16 @@ public:
 	MatrixXd _zDiff;
 };
 
-class Problem {
+class PoissonProblem {
 public:
-	int xlength=0; 
-	int ylength=0;
-	int zlength=0;
-	double dx=1, dy=1, dz=1;
+	int xlength = 0;
+	int ylength = 0;
+	int zlength = 0;
+	double dx = 1, dy = 1, dz = 1;
 	bool is_2d = true;
 	vector<BoundaryCondition> bc;
-	Problem() {};
-	Problem(int x, int y,int z, bool _is_2d) {
+	PoissonProblem() {};
+	PoissonProblem(int x, int y, int z, bool _is_2d) {
 		xlength = x;
 		ylength = y;
 		zlength = z;
@@ -37,16 +37,16 @@ public:
 	}
 	//Check condition if it is capable
 	//Every boundary should have a conition,if not return -1
-	//If all conditions are cauchy condition, then return 2, in this case we should fix the (0,0) to 0
+	//If all conditions are cauchy condition, then return 2, in this case we should fix the (0,0)/(0,0,0) to 0
 	//If it is all fine, then return 1
 	int conditionCheck() {
 		if (is_2d)
 		{
 			vector<bool> _Bd{ false,false,false,false };
-			bool exist_Dirichlet=false;
+			bool exist_Dirichlet = false;
 			for (size_t i = 0; i < bc.size(); i++)
 			{
-				if (bc[i].location<_Bd.size())
+				if (bc[i].location < _Bd.size())
 				{
 					_Bd[bc[i].location] = true;
 					if (bc[i].is_Dirichlet == true)
@@ -56,7 +56,7 @@ public:
 				}
 			}
 			bool is_capable = true;
-			for (size_t i = 0; i <_Bd.size(); i++)
+			for (size_t i = 0; i < _Bd.size(); i++)
 			{
 				is_capable = _Bd[i] && is_capable;
 			}
@@ -78,6 +78,7 @@ public:
 
 		}
 		else
+			//3D problem
 		{
 			vector<bool> _Bd{ false,false,false,false,false,false };
 			bool exist_Dirichlet = false;
@@ -116,10 +117,10 @@ public:
 	}
 };
 
-void preConditioner(Problem pro,MatrixXd &output,VectorXd &rhs) {
+void preConditioner(PoissonProblem pro, MatrixXd& output, VectorXd& rhs) {
 	//The poisson preconditioner
 	int conCheck = pro.conditionCheck();
-	if (conCheck==-1)
+	if (conCheck == -1)
 	{
 		cout << "Boundary condition is not capable " << endl;
 		exit(0);
@@ -128,33 +129,122 @@ void preConditioner(Problem pro,MatrixXd &output,VectorXd &rhs) {
 	{
 		MatrixXd D(pro.xlength - 2, pro.xlength - 2);
 		D.setZero();
-		for (size_t i = 0; i < pro.xlength-2; i++)
+		for (size_t i = 0; i < pro.xlength - 2; i++)
 		{
 			D(i, i + 1) = -1;
 		}
 		D = D + D.transpose();
-		D = D + 4 * MatrixXd::Identity(pro.xlength-2,pro.xlength-2);
-		output.resize((pro.xlength - 2)*(pro.ylength-2), (pro.xlength - 2)* (pro.ylength - 2));
+		D = D + 4 * MatrixXd::Identity(pro.xlength - 2, pro.xlength - 2);
+		output.resize((pro.xlength - 2) * (pro.ylength - 2), (pro.xlength - 2) * (pro.ylength - 2));
 		output.setZero();
-		for (size_t i = 0; i < pro.ylength-2; i++)
-			for (size_t j = 0; j < pro.ylength-2; j++)
-		{	
-			//The matrix is (m-2)*n-2
+		for (size_t i = 0; i < pro.ylength - 3; i++)
+		{
+			//The matrix is (m-2)*(n-2)
+			output.block(i * (pro.xlength - 2), i * (pro.xlength - 2), pro.xlength - 2, pro.xlength - 2) = D;
+			output.block((i + 1) * (pro.xlength - 2), i * (pro.xlength - 2), pro.xlength - 2, pro.xlength - 2) = -MatrixXd::Identity(pro.xlength - 2, pro.xlength - 2);
+			output.block(i * (pro.xlength - 2), (i + 1) * (pro.xlength - 2), pro.xlength - 2, pro.xlength - 2) = -MatrixXd::Identity(pro.xlength - 2, pro.xlength - 2);
+		}
+		output.block((pro.ylength - 2) * (pro.xlength - 2), (pro.ylength - 2) * (pro.xlength - 2), pro.xlength - 2, pro.xlength - 2) = D;
+		MatrixXd temp((pro.xlength - 2) * (pro.ylength - 2), (pro.xlength - 2) * (pro.ylength - 2));
+		for (size_t i = 0; i < pro.xlength - 2; i++)
+		{
+			bool xub = (i < pro.xlength - 3);
+			bool xlb = (i > 1);
+			for (size_t j = 0; j < pro.ylength - 2; j++)
+			{
+				temp(i * j, i * j) = 4;
+				bool yub = (j < pro.ylength - 3);
+				bool ylb = (j > 1);
+				if (ylb)
+				{
+					temp(i * j, i * (j - 1)) = -1;
+				}
+				if (yub)
+				{
+					temp(i * j, i * (j + 1)) = -1;
+				}
+				if (xub)
+				{
+					temp(i * j, (i + 1) * j) = -1;
+				}
+				if (xlb)
+				{
+					temp(i * j, (i - 1) * j) = -1;
+				}
+			}
+		}
+		assert(output == temp);
 
+	}
+	else
+	{
+		output.resize((pro.xlength - 2) * (pro.ylength - 2) * (pro.zlength - 2), (pro.xlength - 2) * (pro.ylength - 2) * (pro.zlength - 2));
+		for (size_t i = 0; i < pro.xlength - 2; i + pro.xlength - 3)
+		{
+			bool xub = (i < pro.xlength - 3);
+			bool xlb = (i > 1);
+			for (size_t j = 0; j < pro.ylength - 2; j + pro.ylength - 3)
+			{
+				bool yub = (j < pro.ylength - 3);
+				bool ylb = (j > 1);
+				for (size_t k = 0; k < pro.zlength - 2; k + pro.zlength - 3)
+				{
+					bool zub = (k < pro.zlength - 3);
+					bool zlb = (k > 1);
+					output(i * j * k, i * j * k) = 6;
+					if (ylb)
+					{
+						output(i * j * k, i * (j - 1) * k) = -1;
+					}
+					if (yub)
+					{
+						output(i * j * k, i * (j + 1) * k) = -1;
+					}
+					if (xub)
+					{
+						output(i * j * k, (i + 1) * j * k) = -1;
+					}
+					if (xlb)
+					{
+						output(i * j * k, (i - 1) * j * k) = -1;
+					}
+					if (zub)
+					{
+						output(i * j * k, i * j * (k + 1)) = -1;
+					}
+					if (zlb)
+					{
+						output(i * j * k, i * j * (k - 1)) = -1;
+					}
+				}
+			}
+		}
+
+		for (size_t i = 1; i < pro.xlength - 3; i++)
+		{
+			for (size_t j = 1; j < pro.ylength - 3; j++)
+			{
+				for (size_t k = 1; k < pro.zlength - 3; k++)
+				{
+					output(i * j * k, i * j * k) = 6;
+					output(i * j * k, i * (j - 1) * k) = -1;
+					output(i * j * k, i * (j + 1) * k) = -1;
+					output(i * j * k, (i + 1) * j * k) = -1;
+					output(i * j * k, (i - 1) * j * k) = -1;
+					output(i * j * k, i * j * (k + 1)) = -1;
+					output(i * j * k, i * j * (k - 1)) = -1;
+				}
+			}
 		}
 	}
-	else 
-	{
-		output.resize((pro.xlength - 2) * (pro.ylength - 2)*(pro.zlength-2), (pro.xlength - 2) * (pro.ylength - 2)*(pro.zlength-2));
 
-	}
-	
+
 }
 
-void conjugateGradient(Problem pro, VectorXd result) {
+void conjugateGradient(PoissonProblem pro, VectorXd result) {
 	//Solving problems AX=B
 	MatrixXd A;
-	VectorXd B,X;
+	VectorXd B, X;
 	for (size_t i = 0; i < pro.bc.size(); i++)
 	{
 		//To do, set rhs
