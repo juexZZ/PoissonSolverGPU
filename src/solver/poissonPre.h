718611b9c0674e99a4e0af39c2848cce6b30eebe
njsyw1997 <44545837+njsyw1997@users.noreply.github.com>
2022-04-12 16:36:42 -0400

    System Matrix Updated

--- src/solver/poissonPre.h
+++ src/solver/poissonPre.h
@@ -28,17 +28,31 @@ public:
 	double dx = 1, dy = 1, dz = 1;
 	bool is_2d = true;
 	vector<BoundaryCondition> bc;
+	VectorXd f; //Divergence of each point
 	PoissonProblem() {};
 	PoissonProblem(int x, int y, int z, bool _is_2d) {
 		xlength = x;
 		ylength = y;
 		zlength = z;
 		is_2d = _is_2d;
+		f.resize((x - 2) * (y - 2) * (z - 2));
+		f.setZero();
 	}
 	//Check condition if it is capable
 	//Every boundary should have a conition,if not return -1
 	//If all conditions are cauchy condition, then return 2, in this case we should fix the (0,0)/(0,0,0) to 0
 	//If it is all fine, then return 1
+	void setDivergence(VectorXd temp) {
+		if (temp.size()!=f.size())
+		{
+			cout << " The giving divergence can not match the dimension of the Matrix " << endl;
+			exit(0);
+		}
+		else
+		{
+			f = temp;
+		}
+	}
 	int conditionCheck() {
 		if (is_2d)
 		{
@@ -125,9 +139,17 @@ void preConditioner(PoissonProblem pro, MatrixXd& output, VectorXd& rhs) {
 		cout << "Boundary condition is not capable " << endl;
 		exit(0);
 	}
+
+	//Assemble A
+	int xl, yl, zl;
+	xl = pro.xlength-2;
+	yl = pro.ylength-2;
+	zl = pro.zlength - 2;
+	Vector3i base(yl * zl, zl, 1);
 	if (pro.is_2d)
 	{
 		MatrixXd D(pro.xlength - 2, pro.xlength - 2);
+		MatrixXd temp((pro.xlength - 2) * (pro.ylength - 2), (pro.xlength - 2) * (pro.ylength - 2));
 		D.setZero();
 		for (size_t i = 0; i < pro.xlength - 2; i++)
 		{
@@ -135,50 +157,53 @@ void preConditioner(PoissonProblem pro, MatrixXd& output, VectorXd& rhs) {
 		}
 		D = D + D.transpose();
 		D = D + 4 * MatrixXd::Identity(pro.xlength - 2, pro.xlength - 2);
-		output.resize((pro.xlength - 2) * (pro.ylength - 2), (pro.xlength - 2) * (pro.ylength - 2));
-		output.setZero();
+		temp.setZero();
 		for (size_t i = 0; i < pro.ylength - 3; i++)
 		{
 			//The matrix is (m-2)*(n-2)
-			output.block(i * (pro.xlength - 2), i * (pro.xlength - 2), pro.xlength - 2, pro.xlength - 2) = D;
-			output.block((i + 1) * (pro.xlength - 2), i * (pro.xlength - 2), pro.xlength - 2, pro.xlength - 2) = -MatrixXd::Identity(pro.xlength - 2, pro.xlength - 2);
-			output.block(i * (pro.xlength - 2), (i + 1) * (pro.xlength - 2), pro.xlength - 2, pro.xlength - 2) = -MatrixXd::Identity(pro.xlength - 2, pro.xlength - 2);
+			temp.block(i * (pro.xlength - 2), i * (pro.xlength - 2), pro.xlength - 2, pro.xlength - 2) = D;
+			temp.block((i + 1) * (pro.xlength - 2), i * (pro.xlength - 2), pro.xlength - 2, pro.xlength - 2) = -MatrixXd::Identity(pro.xlength - 2, pro.xlength - 2);
+			temp.block(i * (pro.xlength - 2), (i + 1) * (pro.xlength - 2), pro.xlength - 2, pro.xlength - 2) = -MatrixXd::Identity(pro.xlength - 2, pro.xlength - 2);
 		}
-		output.block((pro.ylength - 2) * (pro.xlength - 2), (pro.ylength - 2) * (pro.xlength - 2), pro.xlength - 2, pro.xlength - 2) = D;
-		MatrixXd temp((pro.xlength - 2) * (pro.ylength - 2), (pro.xlength - 2) * (pro.ylength - 2));
+		temp.block((pro.ylength - 2) * (pro.xlength - 2), (pro.ylength - 2) * (pro.xlength - 2), pro.xlength - 2, pro.xlength - 2) = D;
+		output.resize((pro.xlength - 2) * (pro.ylength - 2), (pro.xlength - 2) * (pro.ylength - 2));
 		for (size_t i = 0; i < pro.xlength - 2; i++)
 		{
 			bool xub = (i < pro.xlength - 3);
 			bool xlb = (i > 1);
 			for (size_t j = 0; j < pro.ylength - 2; j++)
 			{
-				temp(i * j, i * j) = 4;
+				output(i * j, i * j) = 2.0/pro.dx/pro.dx+2.0/pro.dy/pro.dy;
 				bool yub = (j < pro.ylength - 3);
 				bool ylb = (j > 1);
 				if (ylb)
 				{
-					temp(i * j, i * (j - 1)) = -1;
+					output((i * yl) + j, i * yl + (j - 1)) = -1/pro.dy/pro.dy;
 				}
 				if (yub)
 				{
-					temp(i * j, i * (j + 1)) = -1;
+					output((i * yl) + j, i * yl + (j + 1)) = -1/pro.dy/pro.dy;
 				}
 				if (xub)
 				{
-					temp(i * j, (i + 1) * j) = -1;
+					output(i * yl + j, (i + 1) * yl + j) = -1/pro.dx/pro.dx;
 				}
 				if (xlb)
 				{
-					temp(i * j, (i - 1) * j) = -1;
+					output(i * yl + j, (i - 1) * yl + j) = -1/pro.dx/pro.dx;
 				}
 			}
 		}
-		assert(output == temp);
+		assert(output.isApprox(temp));
 
 	}
 	else
 	{
 		output.resize((pro.xlength - 2) * (pro.ylength - 2) * (pro.zlength - 2), (pro.xlength - 2) * (pro.ylength - 2) * (pro.zlength - 2));
+		double dx2, dy2, dz2;
+		dx2 = pro.dx * pro.dx;
+		dy2 = pro.dy * pro.dy;
+		dz2 = pro.dz * pro.dz;
 		for (size_t i = 0; i < pro.xlength - 2; i + pro.xlength - 3)
 		{
 			bool xub = (i < pro.xlength - 3);
@@ -191,30 +216,31 @@ void preConditioner(PoissonProblem pro, MatrixXd& output, VectorXd& rhs) {
 				{
 					bool zub = (k < pro.zlength - 3);
 					bool zlb = (k > 1);
-					output(i * j * k, i * j * k) = 6;
+					Vector3i  coor(i, j, k);
+					output(coor.dot(base),coor.dot(base)) =2.0/dx2+2.0/dy2+2.0/dz2;
 					if (ylb)
 					{
-						output(i * j * k, i * (j - 1) * k) = -1;
+						output(coor.dot(base), (coor+Vector3i(0,-1,0)).dot(base)) = -1/dy2;
 					}
 					if (yub)
 					{
-						output(i * j * k, i * (j + 1) * k) = -1;
+						output(coor.dot(base), (coor + Vector3i(0, 1, 0)).dot(base)) = -1/dy2;
 					}
 					if (xub)
 					{
-						output(i * j * k, (i + 1) * j * k) = -1;
+						output(coor.dot(base), (coor + Vector3i(1,0, 0)).dot(base)) = -1/dx2;
 					}
 					if (xlb)
 					{
-						output(i * j * k, (i - 1) * j * k) = -1;
+						output(coor.dot(base), (coor + Vector3i(-1, 0, 0)).dot(base)) = -1/dx2;
 					}
 					if (zub)
 					{
-						output(i * j * k, i * j * (k + 1)) = -1;
+						output(coor.dot(base), (coor + Vector3i(0, 0, 1)).dot(base)) = -1/dz2;
 					}
 					if (zlb)
 					{
-						output(i * j * k, i * j * (k - 1)) = -1;
+						output(coor.dot(base), (coor + Vector3i(0, 0, 1)).dot(base)) = -1/dz2;
 					}
 				}
 			}
@@ -226,17 +252,31 @@ void preConditioner(PoissonProblem pro, MatrixXd& output, VectorXd& rhs) {
 			{
 				for (size_t k = 1; k < pro.zlength - 3; k++)
 				{
-					output(i * j * k, i * j * k) = 6;
-					output(i * j * k, i * (j - 1) * k) = -1;
-					output(i * j * k, i * (j + 1) * k) = -1;
-					output(i * j * k, (i + 1) * j * k) = -1;
-					output(i * j * k, (i - 1) * j * k) = -1;
-					output(i * j * k, i * j * (k + 1)) = -1;
-					output(i * j * k, i * j * (k - 1)) = -1;
+					Vector3i  coor(i, j, k);
+					output(coor.dot(base), coor.dot(base)) = 2.0 / dx2 + 2.0 / dy2 + 2.0 / dz2;
+					output(coor.dot(base), (coor + Vector3i(0, -1, 0)).dot(base)) = -1/dy2;
+					output(coor.dot(base), (coor + Vector3i(0, 1, 0)).dot(base)) = -1/dy2;
+					output(coor.dot(base), (coor + Vector3i(1, 0, 0)).dot(base)) = -1/dx2;
+					output(coor.dot(base), (coor + Vector3i(-1, 0, 0)).dot(base)) = -1/dx2;
+					output(coor.dot(base), (coor + Vector3i(0, 0, 1)).dot(base)) = -1/dz2;
+					output(coor.dot(base), (coor + Vector3i(0, 0, -1)).dot(base)) = -1/dz2;
 				}
 			}
 		}
 	}
+
+	// Assemble rhs
+	if (pro.is_2d)
+	{
+		rhs.resize(xl* yl);
+		rhs = pro.f;
+		for (size_t i = 0; i < pro.bc.size(); i++)
+		{
+
+		}
+	}
+
+
 
 
 }
